9e621b94f7336c3e635a809128fb1b1a070ca88d
Luft <6987228+luftkeks@users.noreply.github.com>
2021-10-17 23:35:13 +0200

    notes update

--- 2015/15/cookieScoring.go
+++ 2015/15/cookieScoring.go
@@ -1,8 +1,8 @@
 package main
 
-// Hardcode all the ingredients (as a struct)
+// Hardcode all the ingredients (as a struct) --> dont hardcode just read in the input
 // make a cookie which can hold the ingredients
 // make a function which calculates the score which takes a channel
-// do a for for for for loop which gives all the possible combinations up to 100
+// do a for for for for loop which gives all the possible combinations up to 100 --> can we generalize this?
 // fire the function with the cookie channel
 // check the output of the channel for score - save the highest scoring.
