df9bca6ba79d6764b4ee397aea5135189028ce86
Luft <6987228+luftkeks@users.noreply.github.com>
2021-10-17 23:33:36 +0200

    some notes for 2015/15  because its to late to realy solve it

---
+++ 2015/15/cookieScoring.go
@@ -0,0 +1,8 @@
+package main
+
+// Hardcode all the ingredients (as a struct)
+// make a cookie which can hold the ingredients
+// make a function which calculates the score which takes a channel
+// do a for for for for loop which gives all the possible combinations up to 100
+// fire the function with the cookie channel
+// check the output of the channel for score - save the highest scoring.
