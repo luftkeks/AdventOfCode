a649f60669e9ebd7a796ca9d8680d1c5dac61135
Luft <keksluft@gmail.com>
2021-12-09 10:21:32 +0100

    many errors but have to work

--- 2021/09/caveHeight.go
+++ 2021/09/caveHeight.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 )
 
 type point struct {
 	xx, yy, height int
+}
+
+type basin struct {
+	points []point
 }
 
 func elapsed() func() {
@@ -19,7 +24,7 @@ func elapsed() func() {
 
 func main() {
 	defer elapsed()()
-	dat, err := os.Open("input.txt")
+	dat, err := os.Open("test.txt")
 	if err != nil {
 		panic("Hilfe File tut nicht")
 	}
@@ -32,7 +37,7 @@ func main() {
 	}
 
 	heightMap := make([][]int, len(lines))
-	for index, _ := range heightMap {
+	for index := range heightMap {
 		heightMap[index] = make([]int, len(lines[index]))
 		for indexLine, char := range lines[index] {
 			number, err := strconv.Atoi(string(char))
@@ -44,8 +49,36 @@ func main() {
 	}
 
 	lowestPoints := []point{}
+	basins := []basin{}
 	for yy := 0; yy < len(heightMap); yy++ {
 		for xx := 0; xx < len(heightMap[yy]); xx++ {
+
+			dot := point{xx: xx, yy: yy, height: heightMap[yy][xx]}
+
+			if dot.height < 9 {
+				hasBasin := false
+				bas := -1
+				for ii := 0; ii < len(basins); ii++ {
+					if hasBasin {
+						if isPointAdjectedToBasin(dot, basins[ii]) {
+							basins[ii].points = append(basins[ii].points, basins[bas].points...)
+						}
+						continue
+					}
+
+					isInBasin := isPointAdjectedToBasin(dot, basins[ii])
+					if isInBasin {
+						basins[ii].points = append(basins[ii].points, dot)
+						bas = ii
+					}
+					hasBasin = isInBasin
+				}
+				if !hasBasin {
+					dots := []point{dot}
+					basins = append(basins, basin{points: dots})
+				}
+			}
+
 			if numberNotOverEdge(heightMap, yy, xx-1) && heightMap[yy][xx-1] <= heightMap[yy][xx] {
 				continue
 			}
@@ -59,7 +92,7 @@ func main() {
 				continue
 			}
 
-			lowestPoints = append(lowestPoints, point{xx: xx, yy: yy, height: heightMap[yy][xx]})
+			lowestPoints = append(lowestPoints, dot)
 		}
 	}
 
@@ -70,10 +103,32 @@ func main() {
 
 	fmt.Printf("The risk level of the lowest points in the map is: %v\n", riskLevel)
 
-	for _, dot := range lowestPoints {
-		basin := []point{}
-
+	// cleanup Basins
+	resultBasins := []basin{}
+	for _, bas := range basins {
+		isInResult := false
+		for _, resBas := range resultBasins {
+			if resBas.checkPoint(bas.points[0]) {
+				isInResult = true
+			}
+		}
+		if !isInResult {
+			resultBasins = append(resultBasins, bas)
+		}
 	}
+
+	sort.SliceStable(resultBasins, func(i, j int) bool { return len(resultBasins[i].points) > len(resultBasins[j].points) })
+
+	fmt.Println(resultBasins)
+}
+
+func (b *basin) checkPoint(dot point) bool {
+	for _, thing := range b.points {
+		if isPointAdjected(thing, dot) {
+			return true
+		}
+	}
+	return false
 }
 
 func numberNotOverEdge(maap [][]int, yy, xx int) bool {
@@ -87,9 +142,33 @@ func numberNotOverEdge(maap [][]int, yy, xx int) bool {
 	return true
 }
 
-func isPointInStruct(list []point, dot point) bool {
+func isPointInSlice(list []point, dot point) bool {
 	for _, element := range list {
 		if element == dot {
+			return true
+		}
+	}
+	return false
+}
+
+func isPointAdjected(point1, point2 point) bool {
+	if Abs(point1.xx-point2.xx)+Abs(point1.yy-point2.yy) == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func isPointAdjectedToBasin(dot point, bas basin) bool {
+	for _, basinDot := range bas.points {
+		if isPointAdjected(dot, basinDot) {
 			return true
 		}
 	}
