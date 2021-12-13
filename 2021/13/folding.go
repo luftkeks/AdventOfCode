20ca9f86212d767a24cc275355204cbc713f14bb
Luft <6987228+luftkeks@users.noreply.github.com>
2021-12-13 08:20:28 +0100

    solved part two of 21/13 and loved it

--- 2021/13/folding.go
+++ 2021/13/folding.go
@@ -12,8 +12,27 @@ import (
 func main() {
 	defer elapsed()()
 
-	dots := parseWithFoldings("input.txt", 1)
-	fmt.Println(dots)
+	dots := len(parseWithFoldings("input.txt", 1))
+	fmt.Printf("The Answer to Part one is: %v\n", dots)
+
+	points := parseWithFoldings("input.txt", 100)
+	xMax, yMax := Max(points)
+	lines := make([][]rune, yMax+1)
+	for ii := 0; ii < len(lines); ii++ {
+		line := []rune{}
+		for jj := 0; jj < xMax+1; jj++ {
+			line = append(line, ' ')
+		}
+		lines[ii] = line
+	}
+	for _, dot := range points {
+		lines[dot.y][dot.x] = '#'
+	}
+	str := []string{}
+	for _, line := range lines {
+		str = append(str, string(line)+"\n")
+	}
+	fmt.Println(str)
 }
 
 func parseWithFoldings(input string, folding int) []Point {
@@ -41,13 +60,23 @@ func parseWithFoldings(input string, folding int) []Point {
 			foldings = append(foldings, Folding{direction: split3[0], line: number})
 		}
 	}
-	return len(fold(points, foldings[0]))
+	for ii := 0; ii < folding && ii < len(foldings); ii++ {
+		points = fold(points, foldings[ii])
+	}
+	return points
 }
 
 func fold(points []Point, fold Folding) []Point {
 	pointSet := map[Point]bool{}
 	if fold.direction == "y" {
-
+		for _, dot := range points {
+			if dot.y < fold.line {
+				pointSet[dot] = true
+			} else {
+				dotNew := Point{y: 2*fold.line - dot.y, x: dot.x}
+				pointSet[dotNew] = true
+			}
+		}
 	} else if fold.direction == "x" {
 		for _, dot := range points {
 			if dot.x < fold.line {
@@ -78,4 +107,18 @@ type Point struct {
 func elapsed() func() {
 	start := time.Now()
 	return func() { fmt.Printf("Day took %v\n", time.Since(start)) }
+}
+
+func Max(points []Point) (int, int) {
+	var maxX int = points[0].x
+	var maxY int = points[0].y
+	for _, value := range points {
+		if maxX < value.x {
+			maxX = value.x
+		}
+		if maxY < value.y {
+			maxY = value.y
+		}
+	}
+	return maxX, maxY
 }
