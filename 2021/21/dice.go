9d1f306774f462259961118703d586ea3b97d73a
luftkeks <keksluft@gmail.com>
2021-12-24 15:45:09 +0100

    solved 21/21 this bug was stupid

--- 2021/21/dice.go
+++ 2021/21/dice.go
@@ -14,19 +14,9 @@ func elapsed() func() {
 	return func() { fmt.Printf("Day took %v\n", time.Since(start)) }
 }
 
-type DeterministicDice struct {
-	number, timesRolled int
-}
-
-func (d *DeterministicDice) get() int {
-	d.number = (d.number)%100 + 1
-	d.timesRolled++
-	return d.number
-}
-
 func main() {
 	defer elapsed()()
-	dat, err := os.Open("test.txt")
+	dat, err := os.Open("input.txt")
 	if err != nil {
 		panic("Hilfe File tut nicht")
 	}
@@ -53,28 +43,31 @@ func main() {
 	partOne(number1, number2)
 
 	multimap := map[int]int{3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
-	player1, player2 := startRound(number1, number2, 0, 0, 1, &multimap)
+	player1, player2 := 0, 0
+	for winCond := 0; winCond < 22; winCond++ {
+		player1, player2 = startRound(winCond, number1, number2, 0, 0, 1, &multimap)
+		fmt.Printf("times won winCond: %v player1: %v player2: %v\n", winCond, player1, player2)
+	}
 
-	fmt.Printf("times won player1: %v player2: %v\n", player1, player2)
 	fmt.Printf(" vgl mit lösung 1: %v       2: %v\n", player1-444356092776315, player2-341960390180808)
 }
 
-func startRound(number1old, number2old, score1old, score2old int, multiplicator int, multimap *map[int]int) (int, int) {
-	winCondition := 21
+func startRound(winCond, number1old, number2old, score1old, score2old int, multiplicator int, multimap *map[int]int) (int, int) {
+	winCondition := winCond
 	player1won := 0
 	player2won := 0
 	for dice1 := 3; dice1 <= 9; dice1++ {
+		number1new, score1new, multi1New := doStep(dice1, number1old, score1old, multiplicator, multimap)
+		if score1new >= winCondition {
+			player1won += multi1New
+			continue
+		}
 		for dice2 := 3; dice2 <= 9; dice2++ {
-			number1new := (number1old-1+dice1)%10 + 1
-			number2new := (number2old-1+dice2)%10 + 1
-			score1new := score1old + number1new
-			score2new := score2old + number2new
-			if score1new >= winCondition {
-				player1won += multiplicator * (*multimap)[dice1]
-			} else if score2new >= winCondition {
-				player2won += multiplicator * (*multimap)[dice1] * (*multimap)[dice2]
+			number2new, score2new, multi2New := doStep(dice2, number2old, score2old, multi1New, multimap)
+			if score2new >= winCondition {
+				player2won += multi2New
 			} else {
-				player1wonTemp, player2wonTemp := startRound(number1new, number2new, score1new, score2new, multiplicator*(*multimap)[dice1]*(*multimap)[dice2], multimap)
+				player1wonTemp, player2wonTemp := startRound(winCond, number1new, number2new, score1new, score2new, multi2New, multimap)
 				player1won += player1wonTemp
 				player2won += player2wonTemp
 			}
@@ -83,8 +76,11 @@ func startRound(number1old, number2old, score1old, score2old int, multiplicator 
 	return player1won, player2won
 }
 
-func doStep(dice, number, score, multi int, multimap *map[int]int) (scoreNew, numberNew, multiNew int) {
-
+func doStep(dice, number, score, multi int, multimap *map[int]int) (numberNew, scoreNew, multiNew int) {
+	numberNew = (number-1+dice)%10 + 1
+	scoreNew = score + numberNew
+	multiNew = multi * (*multimap)[dice]
+	return
 }
 
 func partOne(number1, number2 int) {
@@ -109,4 +105,14 @@ func partOne(number1, number2 int) {
 			return
 		}
 	}
+}
+
+type DeterministicDice struct {
+	number, timesRolled int
+}
+
+func (d *DeterministicDice) get() int {
+	d.number = (d.number)%100 + 1
+	d.timesRolled++
+	return d.number
 }
